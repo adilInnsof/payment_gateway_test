8fd09630b9fcbc158c37dcf4ef8eae8653f4ed7d
adilmhd4539 <adil@innsof.com>
2024-11-05 10:35:45 +0530

    changed ui for better understanding

--- lib/main.dart
+++ lib/main.dart
@@ -37,93 +37,58 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  initiatePayment() async {
+  String errorCode = "";
+  String message = "";
+  checkPayment() async {
     MFInitiatePaymentRequest request = MFInitiatePaymentRequest(
         invoiceAmount: 10, currencyIso: MFCurrencyISO.SAUDIARABIA_SAR);
-    await MFSDK
-        .initiatePayment(request, MFLanguage.ENGLISH)
-        .then((value) => print(value.paymentMethods.toString()))
-        .catchError((error) => {print(error.message)});
+    await MFSDK.initiatePayment(request, MFLanguage.ENGLISH).then((value) {
+      print(value.paymentMethods.toString());
+    }).catchError((error) {
+      print(error.message);
+      if (error is MFError) {
+        setState(() {
+          errorCode = error.code ?? "";
+          message = error.message ?? "";
+        });
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SizedBox(
+        width: double.infinity,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: checkPayment, child: const Text('checkPayment')),
+              const SizedBox(
+                height: 10,
+              ),
+              Text("errcode: $errorCode"),
+              const SizedBox(
+                height: 10,
+              ),
+              Text("message: $message"),
+            ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: initiatePayment,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
